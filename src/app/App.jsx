e1b05e70e6e7a8f0d64122e8144e1bceb7fa1619
Sean Astrakhan <sastrakhan@gmail.com>
2017-02-21 21:23:16 -0500

    Add Friendlier Welcome message (#6)
    Added the word buddy to the end of the welcome message

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className={style.testing}>
       <h1>Civic Idea Hub</h1>
-      <p>Edit App.jsx to start creating your app</p>
+      <p>Edit App.jsx to start creating your app buddy</p>
     </div>
   );
 }
