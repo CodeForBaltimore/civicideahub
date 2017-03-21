d4b4cd19ef9caad9c888df40a974763370cae613
Sean Astrakhan <sastrakhan@gmail.com>
2017-03-21 18:32:51 -0400

    Added header (#11)
    * added header files for inital commit
    
    * added header files for inital commit
    
    * Added header
    
    * Added .DS_Store to .gitignore
    
    * removed the .DS_Store that was already in there
    
    * added our header to the App, and some NEW!! text

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -3,17 +3,22 @@ import React from "react";
 import StatelessTemplate from '../components/templates/statelesstemplate/stateless.react.jsx';
 import StatefulTemplate from '../components/templates/StatefulTemplate/Stateful.react.jsx';
 
+import Header from "../components/header/header.react.jsx"
+
 import style from "./style.css";
 
 function App() {
   return (
-    <div className={style.testing}>
-      <StatelessTemplate />
-      <StatefulTemplate />
-      <h1>Civic Idea Hub</h1>
-      <p>Edit App.jsx to start creating your app</p>
-      <h1>Civic Idea Hub</h1>
-      <p>Edit App.jsx to start creating your app buddy</p>
+    <div>    
+      <Header />    
+      <div className={style.testing}>      
+        <StatelessTemplate />
+        <StatefulTemplate />
+        <h1>Civic Idea Hub</h1>
+        <p>Edit App.jsx to start creating your app</p>
+        <h1>Civic Idea Hub</h1>
+        <p>Edit App.jsx to start creating your app buddy</p>
+      </div>
     </div>
   );
 }
---
+++ src/components/header/header.react.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = (props) => {
+    return (
+        <div>
+            <h1>Welcome to the Site</h1>
+        </div>
+    );
+}
+
+export default Header;
